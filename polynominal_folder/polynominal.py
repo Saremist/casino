497c674094bc50afddee0dca69cff0237be4f20f
Milan <milanw.2001@gmail.com>
2021-11-27 00:14:38 +0100

    finished validate inputs for ints

--- polynominal_folder/polynominal.py
+++ polynominal_folder/polynominal.py
@@ -1,7 +1,22 @@
-def add_plus(x):  # returns plus to be written if needed
+def add_sign(x):  # returns plus to be written if needed
     if x >= 0:
         return "+"
-    return ""
+    return "-"
+
+
+def abs_not_one(x):
+    a_x = abs(x)  # abs value of x
+    if a_x == 1:
+        return ""
+    else:
+        return a_x
+
+
+def validate_input(list_to_be_checked):
+    for single_power in list_to_be_checked:
+        for element in single_power:
+            if type(element) != int:
+                raise TypeError("input value is not int type")
 
 
 def transform(powers):
@@ -16,23 +31,24 @@ def transform(powers):
 
 class Polynominal:
     def __init__(self, all_powers=[]):
+        validate_input(all_powers)
         self._powers = sorted(all_powers, key=lambda tup: tup[0], reverse=True)
         self.transformed = transform(self._powers)
-        if min(self.transformed[0]) < 0:  # checks for negative degriee
-            raise ValueError("given degriee was negative")
-        if (0, "") in (self.transformed[1]):
-            raise ValueError("given correct degree with 0 value")
-        pass
+        if self._powers:
+            if min(self.transformed[0]) < 0:  # checks for negative degriee
+                raise ValueError("given degriee was negative")
+            if 0 in (self.transformed[1]):
+                raise ValueError("given correct degree with 0 value")
 
     def _str_(self):
         name = ""
         for power in self._powers:
-            if power[0] == 0:
-                name += f"{add_plus(power[1])}{power[1]}"
+            if abs(power[0]) == 0:
+                name += f"{add_sign(power[1])}{abs_not_one(power[1])}"
             elif power[0] == 1:
-                name += f"{add_plus(power[1])}{power[1]}x"
+                name += f"{add_sign(power[1])}{abs_not_one(power[1])}x"
             else:
-                name += f"{add_plus(power[1])}{power[1]}x^{power[0]}"
+                name += f"{add_sign(power[1])}{abs_not_one(power[1])}x^{power[0]}"
         if name == "":
             name = "0"
         return name.strip("+")  # returns str polynominal description
@@ -57,4 +73,5 @@ class Polynominal:
 
 
 if __name__ == "__main__":
+    print(Polynominal([(1, 5), (3, 2), (5, -1)])._str_())
     pass
--- tests/test_polynominal.py
+++ tests/test_polynominal.py
@@ -1,21 +1,53 @@
-from ..polynominal_folder.polynominal import Polynominal
+from polynominal_folder.polynominal import Polynominal
 import pytest
 
 
-def test_Polynominal_str_normal():
+def test_Polynominal_str_normal1():
     assert (Polynominal([])._str_()) == "0"
+
+
+def test_Polynominal_str_normal2():
     assert (Polynominal([(0, -8)])._str_()) == "-8"
-    assert (Polynominal([(0, -4), (1, -2)])._str_()) == "-2x+4"
+
+
+def test_Polynominal_str_normal3():
+    assert (Polynominal([(0, -4), (1, -2)])._str_()) == "-2x-4"
+
+
+def test_Polynominal_str_normal4():
     assert (Polynominal([(1, 5), (3, 2), (5, -1)])._str_()) == "-x^5+2x^3+5x"
+
+
+def test_Polynominal_str_normal5():
     assert (Polynominal([(4, 2), (7, 3), (2, 3), (0, 7)])._str_()) == "3x^7+2x^4+3x^2+7"
 
 
-def test_Polynominal_errors():
+def test_Polynominal_errors1():
     with pytest.raises(ValueError):
         Polynominal([(1, 5), (-3, 7), (5, -1)])
+
+
+def test_Polynominal_errors2():
     with pytest.raises(ValueError):
         Polynominal([(1, 5), (3, 0), (5, -1)])
-    # (Polynominal([])._str_())
-    # (Polynominal([(4, 2), (6, 3), (2, 3), (0, 7)]).degree())
-    # (Polynominal([(4, 1), (6, 5), (2, 3), (11, 7)]).degree())
-    # (Polynominal([(0, 2)]).degree())
+
+
+def test_Polynominal_errors_letters():
+    with pytest.raises(TypeError):
+        Polynominal([("error?", 5), (3, 0), (5, -1)])
+    with pytest.raises(TypeError):
+        Polynominal([(1, 5), (3, "errors!"), (5, -1)])
+    with pytest.raises(TypeError):
+        Polynominal([("error?", 5), (3, 0), (5, "errors")])
+
+
+def test_Polynominal_deg_normal1():
+    assert (Polynominal([(4, 2), (6, 3), (2, 3), (0, 7)]).degree()) == 6
+
+
+def test_Polynominal_deg_normal2():
+    assert (Polynominal([(4, 1), (6, 5), (2, 3), (11, 7)]).degree()) == 11
+
+
+def test_Polynominal_deg_normal_3():
+    assert (Polynominal([(0, 2)]).degree()) == 0
